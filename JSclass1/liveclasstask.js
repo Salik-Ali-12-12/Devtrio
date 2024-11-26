018808b581d53fbe3fc7be2a640fb1e18081be01
Salik-Ali-12 <salikali552@gmailcom>
2024-11-26 14:09:26 -0700

    upload js 1 practice set

--- JSclass1/liveclasstask.js
+++ JSclass1/liveclasstask.js
@@ -1,33 +1,33 @@
-var checkUser = prompt("You have membership:?", "YES");
-var purchaseAmount = +prompt("Enter purchase amount:", 40);
+// var checkUser = prompt("You have membership:?", "YES");
+// var purchaseAmount = +prompt("Enter purchase amount:", 40);
 
-if (checkUser === "YES") {
-    alert("You are member😊");
-    if (purchaseAmount >= 100) {
-        alert("Your purchasing Amount: " + purchaseAmount);
-        alert("You are eligible for 20% discount😊");
-        alert("You pay Amount After Discount: " + (purchaseAmount - (purchaseAmount * 0.2)));
-    } else if (purchaseAmount >= 50 && purchaseAmount <= 99) {
-        alert("Your purchasing Amount: " + purchaseAmount);
-        alert("You are eligible for 10% discount😊");
-        alert("You pay Amount After Discount: " + (purchaseAmount - (purchaseAmount * 0.1)));
-    } else {
-        alert("Your purchasing Amount: " + purchaseAmount);
-        alert("You are eligible for 5% discount😔");
-        alert("You pay Amount After Discount: " + (purchaseAmount - (purchaseAmount * 0.05))); 
-    }
-} else {
-    alert("You are guest");
-    if (purchaseAmount >= 100) {
-       alert("Your purchasing Amount: " + purchaseAmount);
-        alert("You are eligible for 10% discount😊");
-        alert("You pay Amount After Discount: " + (purchaseAmount - (purchaseAmount * 0.1)));
-    } else if (purchaseAmount >= 50 && purchaseAmount <= 99) {
-        alert("Your purchasing Amount: " + purchaseAmount);
-        alert("You are eligible for 5% discount😔");
-        alert("You pay Amount After Discount: " + (purchaseAmount - (purchaseAmount * 0.05))); 
-    } else {
-        alert("Your purchasing Amount: " + purchaseAmount);
-        alert("You are not eligible for any discount😔")
-    }
-}
+// if (checkUser === "YES") {
+//     alert("You are member😊");
+//     if (purchaseAmount >= 100) {
+//         alert("Your purchasing Amount: " + purchaseAmount);
+//         alert("You are eligible for 20% discount😊");
+//         alert("You pay Amount After Discount: " + (purchaseAmount - (purchaseAmount * 0.2)));
+//     } else if (purchaseAmount >= 50 && purchaseAmount <= 99) {
+//         alert("Your purchasing Amount: " + purchaseAmount);
+//         alert("You are eligible for 10% discount😊");
+//         alert("You pay Amount After Discount: " + (purchaseAmount - (purchaseAmount * 0.1)));
+//     } else {
+//         alert("Your purchasing Amount: " + purchaseAmount);
+//         alert("You are eligible for 5% discount😔");
+//         alert("You pay Amount After Discount: " + (purchaseAmount - (purchaseAmount * 0.05))); 
+//     }
+// } else {
+//     alert("You are guest");
+//     if (purchaseAmount >= 100) {
+//        alert("Your purchasing Amount: " + purchaseAmount);
+//         alert("You are eligible for 10% discount😊");
+//         alert("You pay Amount After Discount: " + (purchaseAmount - (purchaseAmount * 0.1)));
+//     } else if (purchaseAmount >= 50 && purchaseAmount <= 99) {
+//         alert("Your purchasing Amount: " + purchaseAmount);
+//         alert("You are eligible for 5% discount😔");
+//         alert("You pay Amount After Discount: " + (purchaseAmount - (purchaseAmount * 0.05))); 
+//     } else {
+//         alert("Your purchasing Amount: " + purchaseAmount);
+//         alert("You are not eligible for any discount😔")
+//     }
+// }
